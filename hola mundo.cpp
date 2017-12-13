f146f031644302f6c32897c2b23a0cf86d5b7b82
GIL-HP\Gil_barcenas <glbrtbrcnc@gmail.com>
2017-12-12 18:30:44 -0600

    Funcion suma

--- hola mundo.cpp
+++ hola mundo.cpp
@@ -7,6 +7,21 @@ int main() {
 
     cout << "Hola Mundo" << endl;
 
+int a,b,suma;
+
+cout<<"\n ingrese numero a:";
+
+cin>>a;
+
+cout<<"\n ingrese numero b:";
+
+cin>>b;
+
+suma=a+b;
+
+cout<<"\n la suma de "<<a<<"+"<<b<<" es:"<<suma;
+
+
     return 0;
 
 }
